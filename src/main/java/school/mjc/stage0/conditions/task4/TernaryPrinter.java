d02d71759b2c4ada719357e8cedfacdf8b610811
artem_shinkevich <SKILLBOX2021oNe>
2022-11-23 22:37:16 +0300

    all done

--- src/main/java/school/mjc/stage0/conditions/task4/TernaryPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryPrinter.java
@@ -2,5 +2,7 @@ package school.mjc.stage0.conditions.task4;
 
 public class TernaryPrinter {
     public void printWhichIsBigger(int first, int second) {
+        boolean moreOrNo = first > second;
+        System.out.println(moreOrNo ? "first" : "second");
     }
 }
